4eec00a103744575747aad6c5c41acdd609e5d0a
Nathaniel Nam <hooray@Nathaniels-MacBook-Air.local>
2026-03-29 17:08:12 -0400

    Clean comments and restore clock tick

--- script.js
+++ script.js
@@ -16,6 +16,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const dateEl = document.getElementById('current-date');
     if (dateEl) {
-        dateEl.innerText = "14th Century"
+        const updateTime = () => {
+            dateEl.innerText = `LOG DATE: 14th Century | TIME: ${new Date().toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute: '2-digit' })} VERONA STANDARD`;
+        };
+        updateTime();
+        setInterval(updateTime, 1000);
     }
 });
